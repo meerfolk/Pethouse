0c58978537e0d7007a6bd19e969c9b6afe2c2469
Waterdark <shadow_priest@mail.ru>
2019-12-04 11:54:31 +0500

    Функция генератор рандомной строки. Принимает 2 аргумента, 1 - целочисленный, 2 - строку

--- server/src/method_random_string.py
+++ server/src/method_random_string.py
@@ -5,7 +5,5 @@ def randomString(long, symbol):
     symbolList = []
     for index in range(1, long + 1):
         symbolList.append(random.choice(symbol))
-    symbol = ''.join(symbolList)
-    return symbol
-
+    return ''.join(symbolList)
 
