11a11c072b1be13f5886b2e7c1a6981a3b963808
Waterdark <shadow_priest@mail.ru>
2019-12-09 13:40:34 +0500

    Функция генератор рандомной строки. Принимает 2 аргумента, 1 - целочисленный, 2 - строку

---
+++ server/src/routes/login_test.py
@@ -0,0 +1,13 @@
+import pytest
+from src.app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    return app
+
+
+def test_login(client):
+    response = client.get('/api/login')
+    assert response.status_code == 200
