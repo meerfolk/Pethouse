33be2a40451e07c602fa0d9622e7a321ee46943b
meerfolk <meerfolk@gmail.com>
2020-01-04 22:14:03 +0300

    add method to get flask app

--- server/src/app.py
+++ server/src/app.py
@@ -2,14 +2,23 @@ from flask import Flask
 from src import routes
 from src.config import init as init_config
 
+app = None
 
 def register_blueprints(app):
   app.register_blueprint(routes.blueprint, url_prefix='/api')
 
+def get_app():
+  if (app != None):
+    return app
+  else:
+    return create_app()
+
 def create_app():
   init_config()
 
+  global app
   app = Flask(__name__)
+
   register_blueprints(app)
 
   return app
