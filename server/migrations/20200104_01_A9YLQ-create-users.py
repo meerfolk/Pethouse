061207572660d6ab7594cba7f498b555afcd1590
meerfolk <meerfolk@gmail.com>
2020-01-05 21:32:48 +0300

    add users table create migration

---
+++ server/migrations/20200104_01_A9YLQ-create-users.py
@@ -0,0 +1,22 @@
+"""
+Create users
+"""
+
+from yoyo import step
+
+__depends__ = {'20191216_01_I4VxG-create-uuid-ossp'}
+
+step(
+  """
+    CREATE TABLE "users" (
+      user_id UUID PRIMARY KEY,
+      created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+      updated_at TIMESTAMPTZ,
+      github_login VARCHAR(50) NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(100) NOT NULL,
+      avatar_url VARCHAR(100) NOT NULL
+    );
+  """,
+  'DROP TABLE "users"',
+)
