8d00ab7f34280be790d76c13bdbe5b76792ba8ee
meerfolk <meerfolk@gmail.com>
2020-01-09 07:01:58 +0300

    patch user migration

--- server/migrations/20200104_01_A9YLQ-create-users.py
+++ server/migrations/20200104_01_A9YLQ-create-users.py
@@ -12,9 +12,9 @@ step(
       user_id UUID PRIMARY KEY,
       created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
       updated_at TIMESTAMPTZ,
-      github_login VARCHAR(50) NOT NULL,
+      github_login VARCHAR(50) NOT NULL UNIQUE,
       name VARCHAR(255) NOT NULL,
-      email VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL UNIQUE,
       avatar_url VARCHAR(100) NOT NULL
     );
   """,
